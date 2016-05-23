7512c18201004278c4b1f2b0994627af1862f457
Scorpionsc <petruninnikolay@gmail.com>
2016-05-23 17:13:17 +0300

    jquery.number-input.js

--- app/js/jquery.number-input.js
+++ app/js/jquery.number-input.js
@@ -13,10 +13,31 @@
                 _onEvents();
             },
             _onEvents = function () {
-                _body.on( 'keypress', '.number input', function (event) {
+                _body.on( 'keypress', '.number', function (event) {
                     if ( ( event.which != 46 || $( this ).val().indexOf( '.' ) != -1 ) && ( event.which < 48 || event.which > 57 ) ) {
                         event.preventDefault();
                     }
+                } );
+                _body.on( 'paste', '.number', function (event) {
+                    var input = $( this ),
+                        inputVal = null;
+
+                    setTimeout( function () {
+                        inputVal = input.val();
+
+                        if( inputVal ){
+                            inputVal = inputVal.split( ',' ).join( '.' );
+                        }
+
+                        inputVal = parseFloat( inputVal );
+
+                        if ( !isNaN( inputVal ) ){
+                            input.val( parseFloat( inputVal ) );
+                        } else {
+                            input.val( '' );
+                        }
+
+                    } );
                 } );
             };
 
