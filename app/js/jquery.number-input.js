916226449957094546a0544073b456919c6b6257
Scorpionsc <petruninnikolay@gmail.com>
2016-05-23 16:55:27 +0300

    jquery.number-input.js

---
+++ app/js/jquery.number-input.js
@@ -0,0 +1,32 @@
+( function () {
+    $( function () {
+        new Number();
+    } );
+
+    var Number = function () {
+
+        //private properties
+        var _body = $( 'body' );
+
+        //private methods
+        var _constructor = function () {
+                _onEvents();
+            },
+            _onEvents = function () {
+                _body.on( 'keypress', '.number input', function (event) {
+                    console.log( this );
+                    console.log($( this ).val().indexOf( '.' ) != -1 );
+                    if ( ( event.which != 46 || $( this ).val().indexOf( '.' ) != -1 ) && ( event.which < 48 || event.which > 57 ) ) {
+                        event.preventDefault();
+                    }
+                } );
+            };
+
+        //public properties
+
+        //public methods
+
+        _constructor();
+    };
+
+} )();
